3cb0b16613aa1cef24ce81805a97702bfd75157c
Xiaoyu44 <72115908+Xiaoyu44@users.noreply.github.com>
2023-10-30 23:13:07 +0000

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "Xiaoyu44/multimedia-tools:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
